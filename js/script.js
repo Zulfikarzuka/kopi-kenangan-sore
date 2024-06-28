7c0e563df476f09bffb39ac842c8beac0b1b2c17
ZUlfikar Zuka <zulfikar@gmail.com>
2024-06-28 09:29:40 +0700

    Kopi kenangan senja modified ZUKAblog

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hamburger menu di Klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan navbar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Animasi img
+const listiElements = document.querySelector(".listi");
